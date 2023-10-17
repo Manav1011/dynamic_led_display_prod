9742503d3b555f157b2eac3cf6ff48a3c065d1b3
Manav1011 <manavshah1011.ms@gmail.com>
2023-10-17 23:32:05 +0530

    .env

--- dynamic_led_display_prod/dynamic_led_display_prod/settings.py
+++ dynamic_led_display_prod/dynamic_led_display_prod/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv() 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=f8_)lr1u-*2nk0c+_rm+5ta5(p&lqpw-fv--+p(3p*u3)=gs!'
+# SECURITY WARNING: keep the secret key used in production secret!)
+SECRET_KEY = os.environ['SECRET']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
